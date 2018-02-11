9b8a4926817a26bbd1a2d4b803734ff0b1ae7a3a
ffbh <2529026882@qq.com>
2018-02-11 18:06:50 +0800

    20180211 evening hdu

---
+++ hdu/hdu/code/2276.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
+#include <bitset>
+using namespace std;
+
+ifstream in("C:\\input.txt");
+//istream& in = cin;
+
+
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
+
+
+
+int T;
+string data[10010];
+
+void ch(char& c){
+	if (c == '0')
+		c = '1';
+	else
+		c = '0';
+
+}
+
+string Next(string& s,int n){
+	
+	string ret = s;
+	if (s.back() == '1'){
+
+	}
+
+
+
+}
+
+int main(){
+	while (in >> T){
+		M.clear();
+
+
+	}
+	
+
+	return 0;
+}
+
+
+
+
+
--- hdu/hdu/code/2285.cpp
+++ hdu/hdu/code/2285.cpp
@@ -1,46 +1,110 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
 using namespace std;
-int N;
-_int64 num[110];
-_int64 K;
-_int64 fun(string& s){
-	_int64 temp = 0;
-	for (int k = 0; k < s.length(); ++k)
-		temp = temp * 2 + s[k] - '0';
-	return temp;
-}
 
-bool dfs(_int64 pre,int pos){
-	if (pre == K)
-		return 1;
-	if (pos == N)
-		return 0;
-	return dfs(pre, pos + 1) || dfs(pre^num[pos], pos + 1);
-}
+//ifstream in("C:\\input.txt");
+istream& in = cin;
 
 
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
 
-int main(){
-	ifstream in("C:\\temp.txt");
-	int M;
-	while (cin >> M >> N){
-		for (int i = 0; i < N; ++i){
-			string s;
-			cin >> s;
-			num[i] = fun(s);
-		}
-		cin >> M;
-		while (M--){
-			string s1, s2;
-			cin >> s1 >> s2;
-			K = fun(s2);
-			if (dfs(fun(s1),0))
-				cout << "Yes" << endl;
-			else
-				cout << "No" << endl;
+
+int N, M;
+LL h[110];
+
+void add(LL v){
+	for (int i = 60; i >= 0; --i){
+		if (v & ((LL)1) << i){
+			if (h[i] == -1){
+				h[i] = v;
+				break;
+			}
+			else{
+				v ^= h[i];
+			}
 		}
 	}
+}
+
+LL input(){
+	string str;
+	in >> str;
+	LL v = 0;
+	for (char c : str){
+		v = v * 2 + c - '0';
+	}
+	return v;
+}
+
+
+bool judge(LL p){
+	for (int i = 60; i >= 0; --i){
+		if (p & ((LL)1) << i){
+			if (h[i] == -1){
+				return 0;
+			}
+			p ^= h[i];
+		}
+	}
+	return 1;
+}
+int main(){
+	while (in >> N >> M){
+		memset(h, -1, sizeof(h));
+		for (int i = 1; i <= M; ++i){
+			
+			LL v = input();
+			
+			add(v);
+		}
+
+		int Q;
+		in >> Q;
+		while (Q-- > 0){
+			LL a = input();
+			LL b = input();
+			b ^= a;
+			
+			if (judge(b)){
+				printf("Yes\n");
+			}
+			else{
+				printf("No\n");
+			}
+
+		}
+
+
+
+	}
+
+
+
 	return 0;
 }
+
+
+
+
+
