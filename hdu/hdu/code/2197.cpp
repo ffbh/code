404b96f66eb32be6d6a8ba851895ae6834df2d89
ffbh <2529026882@qq.com>
2018-02-13 17:43:40 +0800

    hdu benyuanchuan

--- hdu/hdu/code/2197.cpp
+++ hdu/hdu/code/2197.cpp
@@ -4,61 +4,90 @@
 #include <fstream>
 #include <functional>
 #include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
 using namespace std;
-int N;
-int arr[20010], tail;
-int quickmod(int m,int p,int mod){
+
+//ifstream in("C:\\input.txt");
+istream& in = cin;
+
+
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
+
+int mod = 2008;
+
+int quick_mod(int a,int p,int m){
+	a %= m;
 	int ret = 1;
 	while (p){
-		if (p % 2)
-			ret = (ret*m) % mod;
-		m = (m*m) % mod;
+		if (p & 1)
+			ret = ret*a%m;
+		a = a*a%mod;
 		p /= 2;
 	}
 	return ret;
 }
 
-int submod(int a, int b, int mod){
-	return ((a + mod) - b) % mod;
+void sub(int& a,int b){
+	a %= mod;
+	b %= mod;
+	a -= b;
+	a %= mod;
+	if (a < 0)
+		a += mod;
+}
+
+unordered_map<int, int> M;
+
+int dfs(int n){
+	if (M.count(n)){
+		return M[n];
+	}
+	int S = (int)sqrt(n);
+	int all = quick_mod(2, n, mod);
+	for (int i = 1; i <= S; ++i){
+		if (n % i == 0){
+			sub(all, dfs(i));
+			int p = n / i;
+			if (p != n && p != i){
+				sub(all, dfs(p));
+			}
+		}
+	}
+	return M[n] = all;
 }
 
 int main(){
-	ifstream in("C:\\input.txt");
-	while (cin >> N){
-		if (N <= 0){
-			cout << 0 << endl;
-			continue;
-		}
-		if (N == 1){
-			cout << 2 << endl;
-			continue;
-		}
-		tail = 0;
-		int ans = quickmod(2, N, 2008);
-		int srt = sqrt(N);
-		for (int i = 1; i <= srt; ++i){
-			if (N%i == 0){
-				arr[tail++] = i;
-				if (i != 1)
-					arr[tail++] = N / i;
-			}
-		}
-		sort(arr, arr + tail);
-		tail = unique(arr, arr + tail) - arr;
-		for (int i = 0;i < tail; ++i){
-			bool f = 1;
-			for (int j = i+1; j < tail;++j)
-			if (arr[j] % arr[i] == 0){
-				f = 0;
-				break;
-			}
-			
-			if (f){
-				int d = quickmod(2, arr[i], 2008);
-				ans = submod(ans, d, 2008);
-			}
-		}
-		printf("%d\n", ans);
+
+	M[1] = M[2] = 2;
+	int N;
+	while (in>>N){
+		printf("%d\n", dfs(N));
+
+
+
 	}
+
+
 	return 0;
 }
+
+
+
+
+
