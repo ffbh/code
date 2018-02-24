837facb66a791caeed7628a8a0e401c7d228118e
ffbh <2529026882@qq.com>
2018-02-24 13:46:32 +0800

    zoj 3604

--- zoj/zoj/code/3604.cpp
+++ zoj/zoj/code/3604.cpp
@@ -6,68 +6,89 @@
 #include <algorithm>
 #include <ctime>
 #include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
 using namespace std;
-const long long mod = 1000000007;
-long long C[2010][2010];
-long long dp[2010][2010];
 
-long long qm(long long a, int p){
-	long long ret = 1;
+#ifndef ONLINE_JUDGE
+ifstream in("C:\\input.txt");
+#endif
+
+#ifdef ONLINE_JUDGE
+istream& in = cin;
+#endif
+
+
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
+
+
+int N, S;
+
+LL mod = 1e9 + 7;
+
+void input(){
+	in >> N >> S;
+
+
+}
+
+LL quick_mod(LL a,LL p){
+	a %= mod;
+	LL ret = 1;
 	while (p){
 		if (p & 1)
-			ret = (ret*a) % mod;
-		a = (a*a) % mod;
+			ret = ret*a%mod;
+		a = a*a%mod;
 		p /= 2;
 	}
 	return ret;
 }
 
-void Init(){
-	for (int i = 1; i < 2010; ++i){
-		C[i][0] = C[i][i] = 1;
-	}
-	for (int i = 2; i < 2010;++i)
-	for (int j = 1; j <= i; ++j)
-		C[i][j] = (C[i - 1][j - 1] + C[i - 1][j])%mod;
-
-	dp[2][1] = dp[2][2] = 1;
-	for (int i = 3; i < 150; ++i){
-		dp[i][1] = qm(i, i - 2);
-		for (int j = 2; j <= i;++j){
-			for (int k = 0; k < i-j;++k)
-				dp[i][j] = (dp[i][j] + C[i - j][k]*dp[i - k - 1][j - 1] * dp[k + 1][1]) % mod;
-		}
-	}
-
-
-}
-
 int main(){
-	ifstream in("C:\\input.txt");
-	Init();
-	int T;
-	in >> T;
-	while (T-- > 0){
-		int n, s;
-		in >> n >> s;
-		cout << dp[n][s] << endl;
+
+	int TEST_CASE = 1;
+	in >> TEST_CASE;
+	while (TEST_CASE-- > 0){
+		input();
+		if (N == S){
+			printf("1\n");
+		}
+		else{
+			printf("%lld\n", S * quick_mod(N, N - S - 1) % mod);
+		}
+
+
+
+
+
+
+
+
+
+
+
+
 
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	return 0;
 }
+
+
+
+
+
