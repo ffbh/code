d567789302272ae8e3986b693f3cce59b4118ec4
ffbh <2529026882@qq.com>
2018-03-19 21:02:38 +0800

    zoj 4009 big hole

--- zoj/zoj/code/4009.cpp
+++ zoj/zoj/code/4009.cpp
@@ -18,8 +18,8 @@
 using namespace std;
 
 #ifndef ONLINE_JUDGE
-//ifstream in("C:\\input.txt");
-istream& in = cin;
+ifstream in("C:\\input.txt");
+
 #endif
 
 #ifdef ONLINE_JUDGE 
@@ -39,7 +39,15 @@ int n, q;
 int a[100010];
 int mod = 99971;
 
-
+int getval(int& ret){
+	ret = 0;
+	char c;
+	while ((c = getchar()) == ' ' || c == '\n' || c == '\r');
+	ret = c - '0';
+	while ((c = getchar()) != ' '&&c != '\n'&&c != '\r')
+		ret = ret * 10 + c - '0';
+	return ret;
+}
 
 
 void input(){
@@ -47,12 +55,13 @@ void input(){
 	
 	for (int i = 1; i <= n; ++i){
 		//	in >> a[i];
-		scanf("%d", &a[i]);
+		getval(a[i]);
+	//	scanf("%d", &a[i]);
 	}
 
 }
 
-int sum[48][300010], lazy[300010];
+int sum[48][400010], lazy[400010];
 
 
 
@@ -66,9 +75,11 @@ void build(int root, int L, int R){
 	
 	lazy[root] = 0;
 	if (L == R){
+		
 		sum[0][root] = a[L] % mod;
 		for (int i = 1; i < 48; ++i){
-			sum[i][root] = sum[i - 1][root] * sum[i - 1][root] % mod*sum[i - 1][root] % mod;
+			sum[i][root] =(LL)1* sum[i - 1][root] * sum[i - 1][root]*sum[i - 1][root] % mod;
+			
 		}
 	}
 	else{
@@ -99,7 +110,7 @@ void update(int root, int L, int R,int ll,int rr){
 		return;
 	}
 
-	pushdown(root);
+//	pushdown(root);
 	int mid = (ll +rr) >> 1;
 	if (mid >= L)
 		update(lson, L, R, ll, mid);
@@ -109,30 +120,60 @@ void update(int root, int L, int R,int ll,int rr){
 
 }
 
-int query(int root, int L, int R,int ll,int rr){
+
+
+int query(int root, int L, int R,int ll,int rr,int add){
 	//if (ll > R || rr < L)
 	//	return 0;
 
 	if (ll >= L&&rr <= R){
-		return sum[(lazy[root])%48][root];
+		return sum[(lazy[root]+add)%48][root];
 	}
 
-	pushdown(root);
+//	pushdown(root);
 	int mid = (ll + rr) >> 1;
 	int ans = 0;
 	if (mid >= L)
-		ans += query(lson, L, R, ll, mid);
+		ans += query(lson, L, R, ll, mid,add+lazy[root]);
 	if (mid + 1 <= R)
-		ans += query(rson, L, R, mid + 1, rr);
+		ans += query(rson, L, R, mid + 1, rr,add+lazy[root]);
 	ans %= mod;
-	pushup(root);
+	
+
+//	pushup(root);
 	return ans;
 
 }
 
 
+void get_input(int T){
+	ofstream ou("C:\\input.txt");
+	ou << T << endl;
+	srand(time(0));
+	for (int i = 0; i < T; ++i){
+		int n = 1e5;
+		int m = 1e5;
+		ou << n << " " << m << endl;
+		for (int j = 0; j < n; ++j){
+			ou << abs(rand()) << " ";
+		}
+		ou << endl;
+		for (int j = 0; j < m; ++j){
+			int op = abs(rand()) % 2 + 1;
+			int l = abs(rand()) % n + 1;
+			int r = abs(rand()) % n + 1;
+			if (l>r)
+				swap(l, r);
+			ou << op << " " << l << " " << r << endl;
+		}
+	}
+}
+
 
 int main(){
+
+	/*get_input(5);
+	return 0;*/
 
 	int TEST_CASE = 1;
 	in >> TEST_CASE;
@@ -140,25 +181,29 @@ int main(){
 	while (TEST_CASE-- > 0){
 		input();
 
+		
+
 		build(1, 1, n);
 
 		while (q-- > 0){
 			int op, L, R;
-			//	in >> op >> L >> R;
-			scanf("%d%d%d", &op, &L, &R);
-			
+		//		in >> op >> L >> R;
+		//	scanf("%d%d%d", &op, &L, &R);
+			getval(op);
+			getval(L);
+			getval(R);
 			if (op == 1){
 				update(1, L, R, 1, n);
 			}
 			else{
-				printf("%d\n", query(1, L, R, 1, n));
+			//	int ans = query(1, L, R, 1, n, 0);
+				printf("%d\n", query(1, L, R, 1, n,0));
+			//	printf("%d\n", ans);
 
 			}
-
-
 		}
-
 	}
+	
 	return 0;
 }
 
