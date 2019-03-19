798560546c4721fc391e5bc1c4c80078bdc07b2a
ffbh <2529026882@qq.com>
2019-03-19 22:15:33 +0800

    auto_submit in 2019-03-19 22:03:33

--- hdu/hdu/code/1490.cpp
+++ hdu/hdu/code/1490.cpp
@@ -1,26 +1,79 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
+#include <functional>
 #include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
 using namespace std;
-int data[1010][1010];
-int N;
+
+#ifndef ONLINE_JUDGE
+ifstream in("C:\\input.txt");
+#endif
+
+#ifdef ONLINE_JUDGE
+istream& in = cin;
+#endif
+
+
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
+
+
+int n;
+int a[1010][1010];
+
+
+void input(){
+
+
+
+}
+
 
 int main(){
-	ifstream in("C:\\temp.txt");
-	
-	bool flag;
-	while (cin >> N&&N){
-		flag = 1;
-		for (int i = 0; i < N; ++i){
-			for (int j = 0; j < N; ++j)
-				//in >> data[i][j];
-				scanf("%d", &data[i][j]);
-		}
-		
-			
 
+	while (in >> n && n != 0){
+		for (int i = 1; i <= n;++i)
+		for (int j = 1; j <= n; ++j){
+			in >> a[i][j];
+		}
+		bool ok = 1;
+		for (int i = 2; i <= n; ++i)
+		for (int j = 2; j <= n; ++j){
+			if (a[i][j] + a[i - 1][j - 1] != a[i - 1][j] + a[i][j - 1]){
+				ok = 0;
+			}
+		}
+		if (ok){
+			cout << "homogeneous" << endl;
+		}
+		else{
+
+			cout << "not homogeneous" << endl;
+		}
 
 	}
+
 	return 0;
 }
+
+
+
+
+
