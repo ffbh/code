abc02b6f84dc22f704f38c4c9d734d49d92d8750
ffbh <2529026882@qq.com>
2019-03-31 01:14:33 +0800

    auto_submit in 2019-03-31 01:03:33

---
+++ codeforce/codeforce/code/The Doors.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+ifstream in("C:\\input.txt");
+#endif
+
+#ifdef ONLINE_JUDGE
+istream& in = cin;
+#endif
+
+
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
+
+int n;
+int a[2000010];
+
+
+void input(){
+	in >> n;
+	for (int i = 1; i <= n; ++i)
+		in >> a[i];
+
+
+}
+
+
+int main(){
+
+	int TEST_CASE = 1;
+	//in >> TEST_CASE;
+	while (TEST_CASE-- > 0){
+		input();
+
+		for (int i = n - 1; i >= 1; --i){
+			if (a[i] != a[n]){
+				cout << i << endl;
+				break;
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+	return 0;
+}
+
+
+
+
+
