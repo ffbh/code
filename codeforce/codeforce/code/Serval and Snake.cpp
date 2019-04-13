449b478e09af14d65a678ffd47e144f29d11d3d6
ffbh <2529026882@qq.com>
2019-04-14 00:47:49 +0800

    auto_submit in 2019-04-14 00:04:49

---
+++ codeforce/codeforce/code/Serval and Snake.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <set>
+#include <cassert>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+ifstream in("C:\\input.txt");
+#endif
+
+#ifdef ONLINE_JUDGE
+istream& in = cin;
+#endif
+
+
+typedef long long LL;
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define pb push_back
+#define lson (root<<1)  
+#define rson (root<<1|1)  
+#define FFLUAHALL 	fflush(stdin);fflush(stdout);
+
+void _init(){
+
+
+
+}
+
+void input(){
+
+
+
+}
+
+
+int main(){
+
+	int TEST_CASE = 1;
+	//in >> TEST_CASE;
+	while (TEST_CASE-- > 0){
+		_init();
+		input();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+	return 0;
+}
+
+
+
+
+
