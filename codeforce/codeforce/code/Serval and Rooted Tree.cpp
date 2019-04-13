d5e713048678fd409cff5488f8985b358035fb49
ffbh <2529026882@qq.com>
2019-04-13 23:53:32 +0800

    auto_submit in 2019-04-13 23:04:31

--- codeforce/codeforce/code/Serval and Rooted Tree.cpp
+++ codeforce/codeforce/code/Serval and Rooted Tree.cpp
@@ -47,28 +47,47 @@ int n;
 int mark[300010];
 vector<int> vi[300010];
 int num[300010];
+int del[300010];
 int K;
 
-int dfs(int p, int l, int r){
-	if (vi[p].size() == 0){
+int dfs(int p){
+	/*if (vi[p].size() == 0){
 		assert(l == r);
 		return l;
 	}
+	if (mark[p] == 0){
+		return r - del[p];
+	}
 	int ans = l;
 	for (int son : vi[p]){
-		int ret;
+		int ret = dfs(son, r - num[son] + 1, r);
+		ans = max(ans, ret);
+	}
+	return ans;*/
+	if (vi[p].size() == 0){
+		return 0;
+	}
+	int sum = 0;
+	if (mark[p] == 1){
+		sum = 1e9;
+	}
+	else{
+		sum = vi[p].size() - 1;
+	}
+	if (p == 4){
+		int t= 0;
+	}
+	for (int son : vi[p]){
+		int tt = dfs(son);
 		if (mark[p] == 0){
-			ret = dfs(son, l, l + num[son] - 1);
-			ans = max(ans, ret);
+			sum += tt;
 		}
 		else{
-			ret = dfs(son, r - num[son] + 1, r);
-			ans = max(ans, ret);
+			sum = min(sum, tt);
 		}
 
-
 	}
-	return ans;
+	return sum;
 
 }
 
@@ -76,11 +95,20 @@ int pre_dfs(int p){
 	if (vi[p].empty()){
 		K++;
 		num[p] = 1;
+		del[p] = 0;
 		return num[p];
 	}
 	num[p] = 0;
+	del[p] = 0;
+	if (mark[p] == 0){
+		del[p] = vi[p].size() - 1;
+	}
+	
 	for (int son : vi[p]){
 		num[p] += pre_dfs(son);
+	//	if (mark[p] == 0){
+			del[p] += del[son];
+	//	}
 	}
 	return num[p];
 }
@@ -109,8 +137,7 @@ int main(){
 		K = 0;
 		pre_dfs(1);
 
-
-		cout << dfs(1, 1, num[1]) << endl;
+		cout << K - dfs(1) << endl;
 
 
 
