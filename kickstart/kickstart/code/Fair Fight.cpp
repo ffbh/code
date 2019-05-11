85d2d55b894b79eecd93e19f64250b7b1ce6fcdf
ffbh <2529026882@qq.com>
2019-05-11 13:54:17 +0800

    auto_submit in 2019-05-11 13:05:17

--- kickstart/kickstart/code/Fair Fight.cpp
+++ kickstart/kickstart/code/Fair Fight.cpp
@@ -85,23 +85,26 @@ void input(){
 	}
 
 }
-
-void _init(){
-
-
-
-}
 vector<pii> VV;
-int binary_search_min(int l, int r){
+void _init(){
+	//memset(A, 0, sizeof(A));
+	//memset(B, 0, sizeof(B));
+	//memset(L, 0, sizeof(L));
+	//memset(R, 0, sizeof(R));
+	//memset(Lnum, 0, sizeof(Lnum));
+	//memset(Rnum, 0, sizeof(Rnum));
+	//VV.clear();
+}
 
+
+
+
+int bsl(int l,int d){//value not index
 	int L = 0, R = VV.size() - 1, ans = -1;
 	while (L <= R){
 		int mid = (L + R) / 2;
-		if (l <= VV[mid].first && VV[mid].first <= r){
+		if (l >= VV[mid].first){
 			ans = mid;
-			R = mid - 1;
-		}
-		else if (VV[mid].first < l){
 			R = mid - 1;
 		}
 		else{
@@ -109,118 +112,58 @@ int binary_search_min(int l, int r){
 		}
 	}
 	if (ans == -1){
-		return -1;
+		return d;
 	}
-	if (ans == 0){
-		ans = 1;
-	}
-	else{
-		ans = VV[ans - 1].second + 1;
-	}
-	return ans;
+	return VV[ans - 1].second + 1;
 }
 
-int binary_search_max(int l, int r){
 
+int bsr(int r,int d){
 	int L = 0, R = VV.size() - 1, ans = -1;
 	while (L <= R){
 		int mid = (L + R) / 2;
-		if (l <= VV[mid].first && VV[mid].first <= r){
-			ans = VV[mid].second;
-			L = mid + 1;
-		}
-		else if (VV[mid].first < l){
-			R = mid - 1;
-		}
-		else{
-			L = mid + 1;
-		}
-	}
-	return ans;
-}
-
-int binary_search_min2(int l, int r){
-	int L = 0, R = VV.size() - 1, ans = -1;
-	while (L <= R){
-		int mid = (L + R) / 2;
-		if (l <= VV[mid].first && VV[mid].first <= r){
+		if (r >= VV[mid].first){
 			ans = mid;
 			R = mid - 1;
 		}
-		else if (VV[mid].first < l){
-			R = mid - 1;
-		}
 		else{
 			L = mid + 1;
 		}
 	}
-
 	if (ans == -1){
-		return -1;
+		return d;
 	}
+	return VV[ans - 1].second - 1;
 
-
-
-	if (ans == 0){
-		ans = N;
-	}
-	else{
-		ans = VV[ans - 1].second - 1;
-	}
-	return ans;
-}
-
-int binary_search_max2(int l, int r){
-
-	int L = 0, R = VV.size() - 1, ans = -1;
-	while (L <= R){
-		int mid = (L + R) / 2;
-		if (l <= VV[mid].first && VV[mid].first <= r){
-			ans = VV[mid].second;
-			L = mid + 1;
-		}
-		else if (VV[mid].first < l){
-			R = mid - 1;
-		}
-		else{
-			L = mid + 1;
-		}
-	}
-	return ans;
 }
 
 void getLRM(){
 	VV.clear();
+	VV.push_back(mp(10000000, 0));
 	for (int i = 1; i <= N; ++i){
+	
 		while (!VV.empty() && VV.back().first <= B[i]){
 			VV.pop_back();
 		}
 		VV.push_back(mp(B[i], i));
 		int ll = A[i] - K;
 		int rr = A[i] + K;
-		int lll = binary_search_min(ll, rr);
-		int rrr = binary_search_max(ll, rr);
+		LL lll = bsl(rr, i + 1);
+		LL rrr = bsl(ll - 1, i + 1);
 		assert(lll <= rrr);
-		assert(lll >= 1&&lll <= i || lll==-1);
-		assert(rrr >= 1&&rrr <= i || rrr==-1);
-		if (lll == -1){
-			assert(rrr == -1);
-			Lnum[i] = 0;
-			
-		}
-		else if(L[i] <= lll){
-			L[i] = lll;
-			Lnum[i] = abs(rrr - lll) + 1;
-		}
-		else if (L[i] <= rrr){
-			Lnum[i] = abs(rrr - L[i]) + 1;
+		L[i] = max(L[i], lll);
+		lll = max(lll, L[i]);
+		if (lll <= rrr){
+			Lnum[i] = rrr - lll;
 		}
 		else{
 			Lnum[i] = 0;
 		}
+		
 	}
 
 	VV.clear();
+	VV.push_back(mp(10000000, N + 1));
 	for (int i = N; i >= 1; --i){
 		while (!VV.empty() && VV.back().first <= B[i]){
 			VV.pop_back();
@@ -228,22 +171,13 @@ void getLRM(){
 		VV.push_back(mp(B[i], i));
 		int ll = A[i] - K;
 		int rr = A[i] + K;
-		int rrr = binary_search_min2(ll, rr);
-		int lll = binary_search_max2(ll, rr);
+		LL rrr = bsr(rr, i - 1);
+		LL lll = bsr(ll - 1, i - 1);
 		assert(lll <= rrr);
-		assert(lll >= i&&lll <= N || lll==-1);
-		assert(rrr >= i&&rrr <= N || lll==-1);
-		if (lll == -1){
-			assert(rrr == -1);
-			Rnum[i] = 0;
-			
-		}
-		else if (R[i] >= rrr){
-			R[i] = rrr;
-			Rnum[i] = abs(rrr - lll) + 1;
-		}
-		else if (R[i] >= lll){
-			Rnum[i] = abs(R[i] - lll)  + 1;
+		rrr = min(rrr, R[i]);
+		R[i] = min(rrr, R[i]);
+		if (rrr >= lll){
+			Rnum[i] = rrr - lll;
 		}
 		else{
 			Rnum[i] = 0;
@@ -252,13 +186,15 @@ void getLRM(){
 
 }
 
-int p = 0;
+
+
 void getInput(int n,int m){
 	ofstream out("C:\\input.txt");
 
 	srand(time(0));
 	int k = abs(rand()) % m;
-	cout << "k" << k << endl;
+	//cout << "k" << k << endl;
+	out << 1 << endl;
 	out << n << " " << k << endl;
 	for (int i = 1; i <= n; ++i){
 		out << abs(rand())%m  << " ";
@@ -279,34 +215,84 @@ int main(){
 //	VV.push_back(mp(12, 50));
 //	VV.push_back(mp(10, 30));
 //	VV.push_back(mp(5, 20));*/
+
 //	VV.push_back(mp(20, 20));
 //	VV.push_back(mp(15, 30));
 //	VV.push_back(mp(12, 50));
 //	VV.push_back(mp(10, 100));
 //	VV.push_back(mp(5, 999));
 //
-//	int ll = 5;
-//	int rr = 20;
-////	int rrr = binary_search_min2(ll, rr);
-////	int lll = binary_search_max2(ll, rr);
-//	int lll = binary_search_min(ll, rr);
-//	int rrr = binary_search_max(ll, rr);
-//	cout << lll << " " << rrr << endl;
+	//int ll = 20;
+	//int rr = 20;
+//	int rrr = binary_search_min2(ll, rr);
+//	int lll = binary_search_max2(ll, rr);
+
+	//int lll = binary_search_min(ll, rr);
+	//int rrr = binary_search_max(ll, rr);
+	//cout << lll << " " << rrr << endl;
 
 
 //	return 0;
 
+	//N = 5;
+	//A[1] = 8;
+	//A[2] = 8;
+	//A[3] = 8;
+	//A[4] = 8;
+	//A[5] = 8;
+	//getLR();
+	//for (int i = 1; i <= N; ++i){
+	//	cout << L[i] << " ";
+	//}
+	//cout << endl;
+	//for (int i = 1; i <= N; ++i){
+	//	cout << R[i] << " ";
+	//}
+	//cout << endl;
+
+
+
+	/*1
+		10 1
+		8 2 3 5 1 0 9 4 6 3
+		0 4 3 1 1 7 8 5 8 2*/
+
+
+	//1
+	//	5 18
+	//	55 34 72 23 1
+	//	58 50 23 33 99
+
+
+	////getInput(8, 10);
+//	getInput(5, 100);
 	int CPP = 1;
 	int TEST_CASE = 1;
 	in >> TEST_CASE;
 	while (TEST_CASE-- > 0){
-	//	getInput(10, 1000);
+		
 		_init();
 		input();
 
+		
+		//LL kkk = 0;
+		//for (int i = 1; i <= N; ++i){
+		//	LL sa = -1, sb = -1;
+		//	for (int j = i; j <= N; ++j){
+		//		sa = max(sa, A[j]);
+		//		sb = max(sb, B[j]);
+		//		if (abs(sa - sb) <= K){
+		//			kkk++;
+		//		}
+		//	}
+		//}
+	//	cerr << "kkkk" << kkk << endl;
+
+
+
+
+
 		getLR();
-
-
 		getLRM();
 
 
@@ -317,38 +303,50 @@ int main(){
 			assert(l_num >= 0 && r_num >= 0);
 			assert(Lnum[i] >= 0 && Rnum[i] >= 0);
 	//		assert(Lnum[i] <= l_num && Rnum[i] <= r_num);
+		
 			ans += Lnum[i] * r_num + Rnum[i] * l_num - Lnum[i] * Rnum[i];
 
 		}
 
-		/*for (int i = 1; i <= N / 2; ++i){
-			swap(A[i], A[N - i + 1]);
-			swap(B[i], B[N - i + 1]);
-		}*/
-
-		/*getLR();
-
-
-		getLRM();
-*/
-
-		//LL ans2 = 0;
 		//for (int i = 1; i <= N; ++i){
-		//	LL l_num = i - L[i] + 1;
-		//	LL r_num = R[i] - i + 1;
-		//	assert(l_num >= 0 && r_num >= 0);
-		//	assert(Lnum[i] >= 0 && Rnum[i] >= 0);
-		//	//		assert(Lnum[i] <= l_num && Rnum[i] <= r_num);
-		//	ans2 += Lnum[i] * r_num + Rnum[i] * l_num - Lnum[i] * Rnum[i];
-
+		//	swap(A[i], B[i]);
 		//}
-
-		////assert(ans == ans2);
-		//if (ans != ans2){
-		//	break;
+		//if (kkk != ans){
+		//	cout << kkk << " " << ans << endl;
 		//}
+		//assert(kkk == ans);
+
+	//	getLR();
+	//	getLRM();
+
+
+	//	LL ans2 = 0;
+	//	for (int i = 1; i <= N; ++i){
+	//		LL l_num = i - L[i] + 1;
+	//		LL r_num = R[i] - i + 1;
+	//		assert(l_num >= 0 && r_num >= 0);
+	//		assert(Lnum[i] >= 0 && Rnum[i] >= 0);
+	//		//		assert(Lnum[i] <= l_num && Rnum[i] <= r_num);
+	//		ans2 += Lnum[i] * r_num + Rnum[i] * l_num - Lnum[i] * Rnum[i];
+
+	//	}
+	////	cout << ans << " " << ans2 << endl;
+	//	if (ans != ans2){
+	//		cerr << N << " " << K << endl;
+	//		for (int i = 1; i <= N; ++i){
+	//			cerr << A[i] << " ";
+	//		}
+	//		cerr << endl;
+	//		for (int i = 1; i <= N; ++i){
+	//			cerr << B[i] << " ";
+	//		}
+	//		cerr << endl;
+	//		cerr << "test " << ans << " " << ans2 << endl;
+	//	}
+	//	assert(ans == ans2);
 
 		printf("Case #%d: %lld\n", CPP++, ans);
+
 
 
 
