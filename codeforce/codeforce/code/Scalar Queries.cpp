3c1aacf5cb51502da03c82616a31f8bbd25a50c5
ffbh <2529026882@qq.com>
2019-05-19 01:56:50 +0800

    auto_submit in 2019-05-19 01:05:50

--- codeforce/codeforce/code/Scalar Queries.cpp
+++ codeforce/codeforce/code/Scalar Queries.cpp
@@ -23,6 +23,9 @@ ifstream in("C:\\input.txt");
 
 #ifdef ONLINE_JUDGE
 istream& in = cin;
+#define LYSS_CONNECTCHAR(a,b) a##b
+#define ANNOTATION LYSS_CONNECTCHAR(/,/)
+#define cerr ANNOTATION
 #endif
 
 
@@ -50,6 +53,7 @@ int n;
 pii p[500010];
 LL b[2000010];
 LL c[2000010];
+LL d[2000010];
 LL mod = (LL)1e9 + 7;
 
 
@@ -58,13 +62,13 @@ LL update(int root,int l,int r,int x,int v,LL* tree){
 	if (l <= x && x <= r){
 		if (l == r){
 			assert(tree[root] == 0);
-			tree[root] += v;
+			tree[root] = v;
 		}
 		else{
 			int mid = (l + r) / 2;
 			LL ll = update(lson, l, mid, x, v, tree);
 			LL rr = update(rson, mid+1, r, x, v, tree);
-			tree[root] = ll + rr;
+			tree[root] = (ll + rr) % mod;
 		}
 	}
 	return tree[root];
@@ -79,7 +83,7 @@ LL query(int root,int l,int r,int L,int R,LL* tree){
 	}
 	else{
 		int mid = (L + R) / 2;
-		return query(lson, l, r, L, mid, tree) + query(rson, l, r, mid + 1, R, tree);
+		return (query(lson, l, r, L, mid, tree) + query(rson, l, r, mid + 1, R, tree)) % mod;
 	}
 }
 
@@ -104,6 +108,32 @@ void add(LL &A, LL b){
 	}
 }
 
+
+LL b_force(){
+	LL ans = 0;
+	for (int i = 1; i <= n; ++i){
+		for (int j = i; j <= n; ++j){
+			
+			int tmp[100];
+			for (int k = i; k <= j; ++k){
+				tmp[k - i + 1] = p[k].first;
+			}
+			if (i == 1 && j == 2){
+				int t = 0;
+			}
+			int len = j - i + 1;
+			sort(tmp + 1, tmp + len + 1);
+			LL sum = 0;
+			for (int i = 1; i <= len; ++i){
+				add(sum, tmp[i] * i);
+			}
+	//		cerr << "try " << i << " " << j <<  " " << sum << endl;
+			ans += sum;
+		}
+	}
+	return ans;
+}
+
 int main(){
 
 	int TEST_CASE = 1;
@@ -112,6 +142,7 @@ int main(){
 		_init();
 		input();
 
+		cerr << b_force() << endl;
 
 		sort(p + 1, p + n + 1);
 
@@ -120,14 +151,26 @@ int main(){
 		for (int i = 1; i <= n; ++i){
 			LL val = p[i].first;
 			int idx = p[i].second;
-			update(1, 1, n, idx, idx, b);
-			update(1, 1, n, idx, n - idx + 1, c);
+			
+			
+
+		//	LL l_empty = idx - query(1, 1, idx, 1, n, d);
+		//	LL r_empty = n - idx + 1 - query(1, idx, n, 1, n, d);
+		//	update(1, 1, n, idx, 1, d);
+			int l_empty = idx ;
+			int r_empty = n - idx + 1 ;
+			update(1, 1, n, idx, l_empty, b);
+			update(1, 1, n, idx, r_empty, c);
 			LL lf = query(1, 1, idx, 1, n, b);
 			LL rg = query(1, idx, n, 1, n, c);
-			add(ans, lf*rg%mod*val);
+
+			LL xishu = ((lf - idx)*(n - idx + 1)%mod + idx * rg % mod)% mod;
+			if (i == 3){
+				int t = 0;
+			}
+			add(ans, xishu%mod*val);
 		}
 		cout << ans << endl;
-
 
 
 
