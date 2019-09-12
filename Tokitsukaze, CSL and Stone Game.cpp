da47e3949809e41d2b84b32ee9f661a66d8dc83a
JiaZhi Zhou <2529026882@qq.com>
2019-09-13 03:19:25 +0800

    Update Tokitsukaze, CSL and Stone Game.cpp

--- Tokitsukaze, CSL and Stone Game.cpp
+++ Tokitsukaze, CSL and Stone Game.cpp
@@ -15,20 +15,19 @@
 #include <sstream>
 #include <set>
 #include <cassert>
-#include <deque>
 using namespace std;
-
+ 
 #ifndef ONLINE_JUDGE
 ifstream in("/Users/jiazhi.zhou/CLionProjects/acm/InOutput/input.txt");
 #define Debug(v) cerr<<#v<<"="<<v<<'\n'
 #endif
-
+ 
 #ifdef ONLINE_JUDGE
 istream& in = cin;
 #define Debug(v) ;
 #endif
-
-
+ 
+ 
 typedef long long LL;
 #define pii pair<int,int>
 #define pll pair<LL,LL>
@@ -37,148 +36,86 @@ typedef long long LL;
 #define lson (root<<1)
 #define rson (root<<1|1)
 #define FFLUAHALL fflush(stdin);fflush(stdout);
-
-
-
-int n,k;
-string s;
-int _0_l,_0_r,_1_l,_1_r;
-
+ 
+ 
+ 
+int n;
+int a[100010];
+ 
 void input(){
-    in>>n>>k;
-    in>>s;
-
-
+    in>>n;
+    for(int i=1;i<=n;++i){
+        in>>a[i];
+    }
+ 
+ 
 }
-
-
+ 
 void _init(){
-
-
-
+ 
+ 
+ 
 }
-
-deque<int> L[2],R[2];
-
-int get_first(int now ,int aim,int pos){
-    if(!L[aim].empty()){
-        return L[aim][0];
-    }
-    else if(aim == now){
-        return pos;
-    }
-    else {
-        assert(R[aim].size());
-        return R[aim][0];
-    }
-}
-
-int get_last(int now,int aim,int pos){//and_pos = pos + k - 1
-    if(!R[aim].empty()){
-        return R[aim].back();
-    }
-    else if(aim == now){
-        return pos + k - 1;
-    }
-    else{
-        assert(L[aim].size());
-        return L[aim].back();
-    }
-}
-
-
-bool cale(int now){
-    L[0].clear();
-    R[0].clear();
-    L[1].clear();
-    R[1].clear();
-    for(int i=k+1;i<=n;++i){
-        int k = s[i]-'0';
-        R[s[i]-'0'].push_back(i);
-    }
-    for(int pos = 1 ;pos + k -1 <= n;++pos){
-        int l0 = get_first(now,0,pos);
-        int r0 = get_last(now,1,pos);
-        int l1 = get_first(now,1,pos);
-        int r1 = get_last(now,0,pos);
-        if(r0-l0+1<=k || r1-l1+1<=k){
-            //fail
-        }
-        else{
-            return true;
-        }
-
-        L[s[pos]-'0'].push_back(pos);
-
-        if(R[0].empty()){
-            R[1].pop_front();
-        }
-        else if(R[1].empty()){
-            R[0].pop_front();
-        }
-        else if(R[0][0] < R[1][0]){
-            R[0].pop_front();
-        }
-        else{
-            R[1].pop_front();
-        }
-    }
-    return false;
-}
-
-
+ 
+ 
+ 
 int main(){
-
+ 
 	int TEST_CASE = 1;
 	//in >> TEST_CASE;
 	while (TEST_CASE-- > 0){
 		 _init();
 		input();
-        s = '-' + s;
-        _0_l = _1_l = 1e9;
-        _0_r = _1_r = -1;
+ 
+ 
+        LL val = -1;
+        int odd = 0;
+        bool ok = false;
+        sort(a+1,a+n+1);
+        int num = 0;
         for(int i=1;i<=n;++i){
-            if(s[i] == '0'){
-                _0_l = min(_0_l,i);
-                _0_r = i;
+            if(i > 1 && a[i] == a[i-1]){
+                num++;
+                if(a[i] == 0){
+                    ok = true;
+                }
+                else if(i >= 3 && a[i-2] >= a[i] - 1){
+                    ok = true;
+                }
+ 
+            }
+            if(a[i] & 1)
+                odd++;
+        }
+        odd += n / 2;
+        odd &= 1;
+        if(!ok && num < 2){
+            if(odd == 0){
+                cout<<"cslnb"<<endl;
             }
             else{
-                _1_l = min(_1_l,i);
-                _1_r = i;
+                cout<<"sjfnb"<<endl;
             }
         }
-
-//        cale(0);
-//        cale(0);
-        if(_0_r == -1 || _1_r == -1 || _0_r - _0_l + 1 <= k || _1_r - _1_l + 1 <= k){
-            cout<<"tokitsukaze"<<endl;
-        }
-        else if(!cale(0) && !cale(1)){
-            cout<<"quailty"<<endl;
-        }
         else{
-            cout<<"once again"<<endl;
+            cout<<"cslnb"<<endl;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 	}
-
-
+ 
+ 
 	return 0;
 }
-
-
-
-
-
+ 
+ 
